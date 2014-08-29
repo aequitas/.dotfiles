226b58803550a51524c353a929a0cd2121371df5
Johan Bloemberg <mail@ijohan.nl>
2014-08-30 00:50:29 +0200

    bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod a+x .bin/*
-
 sudo defaults write com.apple.loginwindow LoginHook ~/login_script.sh
 sudo defaults write com.apple.loginwindow LogoutHook ~/logout_script.sh
+
+brew install lesspipe 
